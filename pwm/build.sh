1ed096b0b86bd4ac5573b4d59456ba20d6f6aceb
Thierry Reding <treding@nvidia.com>
2015-10-06 15:41:08 +0200

    pwm: Add more configurations
    Add a couple more configurations and fix existing ones to get better
    compile coverage of scripts.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- pwm/build.sh
+++ pwm/build.sh
@@ -2,14 +2,16 @@
 
 PATH=$HOME/pbs-stage1/bin:$PATH
 DIRECTORY="${0%/*}"
+incremental=no
 
 function usage()
 {
 	echo "usage: $1 [options]"
 	echo ""
 	echo "options:"
-	echo "  -h, --help    show this usage information and exit"
-	echo "  -j, --jobs N  build using N parallel jobs"
+	echo "  -h, --help         show this usage information and exit"
+	echo "  -i, --incremental  incremental build"
+	echo "  -j, --jobs N       build using N parallel jobs"
 }
 
 while test $# -ne 0; do
@@ -25,14 +27,24 @@ while test $# -ne 0; do
 			exit 0
 			;;
 
+		--incremental | -i)
+			incremental=yes
+			shift
+			;;
+
 		--jobs | -j)
 			prev=JOBS
 			shift
 			;;
 
-		*)
+		--* | -*)
 			usage $0
-			exit 1;
+			exit 1
+			;;
+
+		*)
+			configs="$configs $1"
+			shift
 			;;
 	esac
 done
@@ -77,6 +89,18 @@ while read NAME ARCH DEFCONFIG OPTIONS; do
 		continue
 	fi
 
+	if test -n "$configs"; then
+		case "$configs" in
+			*$NAME*)
+				;;
+
+			*)
+				continue
+				;;
+
+		esac
+	fi
+
 	OUTPUT="build/$ARCH/$NAME"
 
 	ARGS="ARCH=$ARCH"
@@ -91,8 +115,16 @@ while read NAME ARCH DEFCONFIG OPTIONS; do
 			CROSS_COMPILE=armv7l-unknown-linux-gnueabihf-
 			;;
 
+		arm64)
+			CROSS_COMPILE=aarch64-unknown-linux-gnu-
+			;;
+
 		mips)
 			CROSS_COMPILE=mips-linux-gnu-
+			;;
+
+		unicore32)
+			CROSS_COMPILE=unicore32-linux-
 			;;
 
 		x86 | x86_64)
@@ -112,20 +144,22 @@ while read NAME ARCH DEFCONFIG OPTIONS; do
 
 	echo -en "building \033[33;1m$NAME\033[0m (\033[35;1m$ARCH\033[0m)..."
 
-	mkdir -p "$OUTPUT"
+	if test "x$incremental" = "xno"; then
+		mkdir -p "$OUTPUT"
 
-	if ! make $ARGS "$DEFCONFIG" > "$OUTPUT/build.log" 2>&1; then
-		echo -e "\033[31;1mfailed\033[0m"
-		continue
-	fi
+		if ! make $ARGS "$DEFCONFIG" > "$OUTPUT/build.log" 2>&1; then
+			echo -e "\033[31;1mfailed\033[0m"
+			continue
+		fi
 
-	for option in $OPTIONS; do
-		handle_option "$OUTPUT/.config" $option
-	done
+		for option in $OPTIONS; do
+			handle_option "$OUTPUT/.config" $option
+		done
 
-	if ! make $ARGS olddefconfig <&- < /dev/tty >> "$OUTPUT/build.log" 2>&1; then
-		echo -e "\033[31;1mfailed\033[0m"
-		continue
+		if ! make $ARGS olddefconfig <&- < /dev/tty >> "$OUTPUT/build.log" 2>&1; then
+			echo -e "\033[31;1mfailed\033[0m"
+			continue
+		fi
 	fi
 
 	if ! make $ARGS C=1 -j $JOBS >> "$OUTPUT/build.log" 2>&1; then
