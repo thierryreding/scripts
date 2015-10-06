3a5569509fc367be7bfbea2f94978717b11110a7
Thierry Reding <treding@nvidia.com>
2015-10-06 14:51:41 +0200

    drm: Move configuration into separate files
    Instead of listing configuration options in a variable, simplify things
    by moving them into separate files that can be sourced. This provides a
    script-like way of defining configurations, which will make it possible
    to more flexibly define builds.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drm/build.sh
+++ drm/build.sh
@@ -1,217 +1,138 @@
-#!/bin/sh
+#!/bin/bash
 
-JOBS=13
+basedir="${0%/*}"
+configdir="${basedir}/configs"
+outputdir="build/drm"
 
-x86_options="
-  PCI=y AGP=y USB=y VIRTIO_MMIO=y
-  DRM=y
-  DRM_AMDGPU=y
-  DRM_AMDGPU_CIK=y
-  DRM_AMDGPU_USERPTR=y
-  DRM_AST=y
-  DRM_BOCHS=y
-  DRM_CIRRUS_QEMU=y
-  DRM_GMA500=y
-  DRM_GMA600=y
-  DRM_GMA3600=y
-  DRM_I810=y
-  DRM_I915=y
-  DRM_I915_PRELIMINARY_HW_SUPPORT=y
-  DRM_MGAG200=y
-  DRM_MGA=y
-  DRM_NOUVEAU=y
-  DRM_QXL=y
-  DRM_R128=y
-  DRM_RADEON=y
-  DRM_RADEON_USERPTR=y
-  DRM_RADEON_UMS=y
-  DRM_SAVAGE=y
-  DRM_SIS=y
-  DRM_TDFX=y
-  DRM_UDL=y
-  DRM_VGEM=y
-  DRM_VIA=y
-  DRM_VIRTIO_GPU=y
-  DRM_VMWGFX=y
-  DRM_VMWGFX_FBCON=y
-  DRM_I2C_ADV7511=y
-  DRM_I2C_NXP_TDA998X=y
-"
+verbose=no
+force=no
+JOBS=1
 
-x86_64_options="
-  PCI=y AGP=y USB=y VIRTIO_MMIO=y
-  DRM=y
-  DRM_AMDGPU=y
-  DRM_AMDGPU_CIK=y
-  DRM_AMDGPU_USERPTR=y
-  DRM_AST=y
-  DRM_BOCHS=y
-  DRM_CIRRUS_QEMU=y
-  DRM_GMA500=y
-  DRM_GMA600=y
-  DRM_GMA3600=y
-  DRM_I810=y
-  DRM_I915=y
-  DRM_I915_PRELIMINARY_HW_SUPPORT=y
-  DRM_MGAG200=y
-  DRM_MGA=y
-  DRM_NOUVEAU=y
-  DRM_QXL=y
-  DRM_R128=y
-  DRM_RADEON=y
-  DRM_RADEON_USERPTR=y
-  DRM_RADEON_UMS=y
-  DRM_SAVAGE=y
-  DRM_SIS=y
-  DRM_TDFX=y
-  DRM_UDL=y
-  DRM_VGEM=y
-  DRM_VIA=y
-  DRM_VIRTIO_GPU=y
-  DRM_VMWGFX=y
-  DRM_VMWGFX_FBCON=y
-  DRM_I2C_ADV7511=y
-  DRM_I2C_NXP_TDA998X=y
-"
-
-arm_options="
-  TICK_CPU_ACCOUNTING=y
-  MMU=y
-  ARCH_MULTIPLATFORM=y
-  ARCH_EXYNOS=y
-  ARCH_QCOM=y
-  ARCH_ROCKCHIP=y
-  ARCH_SHMOBILE=y
-  ARCH_TEGRA=y
-  ROCKCHIP_IOMMU=y
-  OMAP2_DSS=y
-  MFD_ATMEL_HLCDC=y
-  IMX_IPUV3_CORE=y
-  STAGING=y
-  DRM=y
-  DRM_ARMADA=y
-  DRM_ATMEL_HLCDC=y
-  DRM_EXYNOS=y
-  DRM_EXYNOS_FIMD=y
-  DRM_EXYNOS5433_DECON=y
-  DRM_EXYNOS7_DECON=y
-  DRM_EXYNOS_DPI=y
-  DRM_EXYNOS_DSI=y
-  DRM_EXYNOS_HDMI=y
-  DRM_EXYNOS_VIDI=y
-  DRM_EXYNOS_G2D=y
-  DRM_EXYNOS_IPP=y
-  DRM_EXYNOS_FIMC=y
-  DRM_EXYNOS_ROTATOR=y
-  DRM_EXYNOS_MIC=y
-  DRM_FSL_DCU=y
-  DRM_IMX=y
-  DRM_IMX_FB_HELPER=y
-  DRM_IMX_PARALLEL_DISPLAY=y
-  DRM_IMX_TVE=y
-  DRM_IMX_LDB=y
-  DRM_IMX_HDMI=y
-  DRM_MSM=y
-  DRM_NOUVEAU=y
-  DRM_OMAP=y
-  DRM_ROCKCHIP=y
-  ROCKCHIP_DW_HDMI=y
-  DRM_SHMOBILE=y
-  DRM_STI=y
-  DRM_STI_FBDEV=y
-  DRM_TEGRA=y
-  DRM_TEGRA_DEBUG=y
-  DRM_TEGRA_STAGING=y
-  DRM_TILCDC=y
-  DRM_VGEM=y
-  DRM_I2C_ADV7511=y
-  DRM_I2C_CH7006=y
-  DRM_I2C_SIL164=y
-  DRM_I2C_NXP_TDA998X=y
-  DRM_PANEL_SIMPLE=y
-  DRM_PANEL_SAMSUNG_S6E8AA0=y
-  DRM_PANEL_SHARP_LQ101R1SX01=y
-  DRM_NXP_PTN3460=y
-  DRM_PARADE_PS8622=y
-"
-
-for config in x86 x86_64 arm; do
-	directory="build/drm/$config"
-	dotconfig="$directory/.config"
-
-	if ! test -d "$directory"; then
-		mkdir -p "$directory"
+while test $# -ne 0; do
+	if test -n "$prev"; then
+		eval "$prev=$1"
+		shift; prev=
+		continue
 	fi
 
-	if ! test -f "$dotconfig"; then
-		eval "options=${config}_options"
-		eval "options=\$${options}"
-
-		make ARCH=$config O="$directory" allnoconfig
-
-		for option in $options; do
-			args="--file $dotconfig"
-			key=${option%%=*}
-			value=${option#*=}
-
-			case $value in
-				y)
-					args="$args --enable $key"
-					;;
-
-				n)
-					args="$args --disable $key"
-					;;
-
-				m)
-					args="$args --module $key"
-					;;
-			esac
-
-			scripts/config $args
-		done
-
-		make ARCH=$config O="$directory" olddefconfig
-	fi
-
-	case $config in
-		x86)
-			CROSS_COMPILE=
+	case $1 in
+		-j | --jobs)
+			prev=JOBS
+			shift
 			;;
 
-		x86_64)
-			CROSS_COMPILE=
+		-f | --force)
+			force=yes
+			shift
 			;;
 
-		arm)
-			CROSS_COMPILE=armv7l-unknown-linux-gnueabihf-
+		-v | --verbose)
+			verbose=yes
+			shift
 			;;
 
 		*)
-			echo "ERROR: unsupported configuration $config"
-			exit 1
+			configs="$configs $1"
+			shift
 			;;
 	esac
-
-	make ARCH=$config CROSS_COMPILE=$CROSS_COMPILE O="$directory" -j $JOBS
 done
 
-#if ! test -f build/drm/x86/.config; then
-#	make ARCH=x86 O=build/drm/x86 allnoconfig
-#fi
-#
-#make ARCH=x86 O=build/drm/x86 -j $JOBS
-#
-#if ! test -f build/drm/x86_64/.config; then
-#	make ARCH=x86 O=build/drm/x86_64 allnoconfig
-#fi
-#
-#make ARCH=x86 O=build/drm/x86_64 -j $JOBS
-#
-#if ! test -f build/drm/arm/.config; then
-#	make ARCH=x86 O=build/drm/arm allnoconfig
-#
-#	kconfig_enable build/drm/arm TICK_CPU_ACCOUNTING
-#fi
-#
-#make ARCH=arm O=build/drm/arm -j $JOBS
+function cross_compile_prepare()
+{
+	local path key value
+
+	if test -f "$HOME/.cross-compile"; then
+		while read key value; do
+			key="${key%:}"
+
+			if test "$key" = "path"; then
+				eval "value=$value"
+
+				if test -n "$path"; then
+					path="$path:$value"
+				else
+					path="$value"
+				fi
+			elif test "$key" = "$ARCH"; then
+				CROSS_COMPILE="$value"
+			fi
+		done < "$HOME/.cross-compile"
+	fi
+
+	if test -n "$path"; then
+		saved_PATH="$PATH"
+		PATH="$path:$PATH"
+	fi
+
+	if test "x$verbose" = "xyes"; then
+		echo "CROSS_COMPILE: $CROSS_COMPILE"
+		echo "PATH: $PATH"
+	fi
+}
+
+function cross_compile_cleanup()
+{
+	if test -n "$saved_PATH"; then
+		PATH="$saved_PATH"
+	fi
+
+	unset CROSS_COMPILE
+}
+
+function kconfig()
+{
+	case $1 in
+		architecture)
+			ARCH=$2
+			;;
+
+		*config)
+			make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE \
+				O=$KBUILD_OUTPUT -j $JOBS $1
+			;;
+
+		enable)
+			scripts/config --file $KBUILD_OUTPUT/.config \
+				--enable $2
+			;;
+
+		module)
+			scripts/config --file $KBUILD_OUTPUT/.config \
+				--module $2
+			;;
+
+		disable)
+			scripts/config --file $KBUILD_OUTPUT/.config \
+				--disable $2
+			;;
+	esac
+}
+
+if test -z "$configs"; then
+	for config in "${configdir}"/*; do
+		configs="$configs ${config##${configdir}/}"
+	done
+fi
+
+for config in $configs; do
+	KBUILD_OUTPUT="${outputdir}/$config"
+	CONFIG_FILE="${configdir}/${config}"
+
+	if test "x$force" = "xyes"; then
+		rm -rf "$KBUILD_OUTPUT"
+	fi
+
+	if ! test -d "$KBUILD_OUTPUT"; then
+		mkdir -p "$KBUILD_OUTPUT"
+	fi
+
+	source "$CONFIG_FILE"
+
+	cross_compile_prepare
+
+	make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE O="$KBUILD_OUTPUT" \
+		-j $JOBS #2>&1 | tee "$directory/build-$(git describe).log"
+
+	cross_compile_cleanup
+done
