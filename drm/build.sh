ec2d3f3318faaec191442b802d8e4c99cda0a5ab
Thierry Reding <treding@nvidia.com>
2015-10-06 15:37:13 +0200

    drm: Create build.log and error.log, support colors
    Write the build output to build.log in the output directory and the
    output of stderr to error.log. The error.log file will be used to check
    if any warnings were encountered and report status differently.
    
    Also add colorized output support (defaults to off, can be enabled with
    the -c, --color option) to make it visually more obvious what the build
    status is.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drm/build.sh
+++ drm/build.sh
@@ -5,8 +5,21 @@ configdir="${basedir}/configs"
 outputdir="build/drm"
 
 verbose=no
+color=no
 force=no
 JOBS=1
+
+function usage()
+{
+	echo "usage: $1 [options] [config...]"
+	echo ""
+	echo "options:"
+	echo "  -c, --color    colorize output"
+	echo "  -f, --force    force build from scratch"
+	echo "  -h, --help     display help screen and exit"
+	echo "  -j, --jobs N   set number of parallel make jobs"
+	echo "  -v, --verbose  increase verbosity"
+}
 
 while test $# -ne 0; do
 	if test -n "$prev"; then
@@ -16,8 +29,8 @@ while test $# -ne 0; do
 	fi
 
 	case $1 in
-		-j | --jobs)
-			prev=JOBS
+		-c | --color)
+			color=yes
 			shift
 			;;
 
@@ -26,9 +39,24 @@ while test $# -ne 0; do
 			shift
 			;;
 
+		-h | --help)
+			usage $0
+			exit 0
+			;;
+
+		-j | --jobs)
+			prev=JOBS
+			shift
+			;;
+
 		-v | --verbose)
 			verbose=yes
 			shift
+			;;
+
+		-*)
+			usage $0
+			exit 1
 			;;
 
 		*)
@@ -37,6 +65,15 @@ while test $# -ne 0; do
 			;;
 	esac
 done
+
+if test "x$color" = "xyes"; then
+	RED="\033[0;31m"
+	GREEN="\033[0;32m"
+	YELLOW="\033[0;33m"
+	MAGENTA="\033[0;35m"
+	CYAN="\033[0;36m"
+	RESET="\033[0m"
+fi
 
 function cross_compile_prepare()
 {
@@ -80,6 +117,14 @@ function cross_compile_cleanup()
 	unset CROSS_COMPILE
 }
 
+function make()
+{
+	(
+		command make "$@" 3>&1 1>&2 2>&3 | tee -a "$KBUILD_OUTPUT/error.log"
+		exit $PIPE_STATUS
+	) &>> "$KBUILD_OUTPUT/build.log"
+}
+
 function kconfig()
 {
 	case $1 in
@@ -115,24 +160,64 @@ if test -z "$configs"; then
 	done
 fi
 
+function log_action()
+{
+	echo -en "${CYAN}$@${RESET} ${MAGENTA}${config}${RESET}..."
+}
+
+function log_status()
+{
+	if test "x$1" != "x0"; then
+		echo -e "${RED}failed ($1)${RESET}"
+		break
+	else
+		if test -s "$KBUILD_OUTPUT/error.log"; then
+			echo -e "${YELLOW}done ($1)${RESET}"
+		else
+			echo -e "${GREEN}done${RESET}"
+		fi
+	fi
+}
+
 for config in $configs; do
 	KBUILD_OUTPUT="${outputdir}/$config"
 	CONFIG_FILE="${configdir}/${config}"
 
 	if test "x$force" = "xyes"; then
+		if test "x$verbose" = "xyes"; then
+			log_action "removing"
+		fi
+
 		rm -rf "$KBUILD_OUTPUT"
+
+		if test "x$verbose" = "xyes"; then
+			log_status $?
+		fi
 	fi
 
 	if ! test -d "$KBUILD_OUTPUT"; then
 		mkdir -p "$KBUILD_OUTPUT"
 	fi
 
-	source "$CONFIG_FILE"
+	if test -f "$KBUILD_OUTPUT/build.log"; then
+		rm "$KBUILD_OUTPUT/build.log"
+	fi
 
+	if test -f "$KBUILD_OUTPUT/error.log"; then
+		rm "$KBUILD_OUTPUT/error.log"
+	fi
+
+	log_action "configuring"
+	source "$CONFIG_FILE"
+	log_status $?
+
+	log_action "building"
 	cross_compile_prepare
 
 	make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE O="$KBUILD_OUTPUT" \
-		-j $JOBS #2>&1 | tee "$directory/build-$(git describe).log"
+		-j $JOBS; rc=$?
 
 	cross_compile_cleanup
+	log_status $rc
+
 done
